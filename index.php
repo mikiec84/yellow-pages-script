82ca1bd26209f0c5d418622719e6a8f10438b58a
Ioannis Kokkinis <info@upg.gr>
2016-05-28 23:49:38 +0300

    Updated index.php.

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if (isset($_GET['search'])) {
 }; ?>
 <!-- Bootstrap -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
+<link rel="stylesheet" href="style.css">
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
