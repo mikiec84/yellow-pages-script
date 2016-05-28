5b4946755beeea0e23ab3e1b95c993b80dec77fa
Ioannis Kokkinis <info@upg.gr>
2016-05-28 23:37:45 +0300

    Updated index.php.

--- api/index.php
+++ api/index.php
@@ -1,8 +1,4 @@
 <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-header('Content-Type: text/html; charset=iso-8859-1');
 include 'db-conf.php';
 include 'db-functions.php';
 include 'functions.php';
