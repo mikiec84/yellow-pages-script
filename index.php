b7e005503bc7074b7133c6bb249867ddc44198c4
Ioannis Kokkinis <info@upg.gr>
2016-05-29 09:11:28 +0300

    Updated index.php.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'settings.php';
 include 'functions.php';
 if (isset($_GET['id'])) {
     $vid = $_GET['id'];
-    loadresult('http://'.$domain.'/api/?type=readresult&id=$vid');
+    loadresult('http://'.$domain.'/api/?type=rGet+US+Email+result&id=$vid');
 }
 if (isset($_GET['search'])) {
     $search = $_GET['search'];
