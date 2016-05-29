530188b8fc48d0c2066842a6199e9c2ea7850b05
Ioannis Kokkinis <info@upg.gr>
2016-05-29 12:33:47 +0300

    Updated functions.php.

--- functions.php
+++ functions.php
@@ -6,7 +6,8 @@ function loadresult($apicall)
     $resultArray = json_decode($json, true);
   //  print_r($resultArray);
     foreach ($resultArray as $key => $value) {
-    return    $$key = $value;
+         $$key = $value;
+         echo $$key;
     }
 }
 
